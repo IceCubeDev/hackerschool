f0c227c7d77bf677688bc1fd5d011acafc26ec51
IceCubeDev <ivandortulov@yahoo.com>
2015-03-02 10:48:33 +0200

    [DBEngine] Added error checking; Fixed file writing with correct data types

--- DBEngine/Perl/PerlDB.pm
+++ DBEngine/Perl/PerlDB.pm
@@ -2,6 +2,9 @@ package PerlDB;
 use warnings;
 use strict;
 use Fcntl;
+use Fcntl 'SEEK_CUR';
+use Fcntl 'SEEK_SET';
+use Fcntl 'SEEK_END';
 
 sub new($$) {
     my $class = shift;
@@ -9,7 +12,8 @@ sub new($$) {
     
     my $self = bless {
         tables => [],
-        current_db => undef
+        current_db => undef,
+        fh => undef
     }, $class;
     
     return $self;
@@ -18,9 +22,12 @@ sub new($$) {
 sub select_db($$) {
     my $self = shift;
     my $db = shift;
-    
-    opendir(my $dir, "Databases/".$db) or die "Unable to select database: ".$!;
-    $self->{current_db} = $db;
+    my $dir;
+    if (!opendir($dir, "Databases/".$db)) {
+        print "[ERROR] Unable to select database '$db': ".$!;
+        return undef;
+    }
+    $self->{current_db} = lc $db;
     
     while (my $file = readdir($dir)) {
         if (not substr($file,0,1) eq "." and $file =~ m/(.*).schema/) {
@@ -36,21 +43,22 @@ sub create_db($$$) {
     my $name = shift;
     my $encoding = shift;
     
-    my $path = "Databases/".$name;
+    my $path = "Databases/".lc $name;
     if (mkdir ($path, 0755)) {
-        sysopen(my $fh, $path."/".$name.".engine", O_WRONLY | O_CREAT, 0755) or
-            die "Unable to open ".$path."/".$name.".engine";
+        if (!sysopen($self->{fh}, $path."/".$name.".engine", O_WRONLY | O_CREAT, 0755)){
+            print "[ERROR] Unable to create database '$name': ".$!."\n";
+            return undef;
+        }
         
-        print "Database '".$name."' created!\n";
-        print $fh "encoding=".$encoding."\n";
-        close $fh;
+        syswrite($self->{fh}, $encoding);
+        close $self->{fh};
     } else {
-        print "[ERROR]Failed to create database '".$name."': ".$!."\n";
-        return 0;
+        print "[ERROR] Unable to create database '$name': ".$!."\n";
+        return undef;
     }
     
     print "CREATE DATABASE\n";
-    return 1;
+    return $name;
 }
 
 sub create_table($$$) {
@@ -58,43 +66,54 @@ sub create_table($$$) {
     
     if (!defined $self->{current_db}) {
         print "[ERROR] No database selected!\n";
-        return 0;
+        return undef;
     }
     
-    my ($key, $value);
-    
-    my $path = lc "Databases\\".$self->{current_db}."\\".$table.".schema";
-    
-    if (-e $path) {
+    my $schema_path = "Databases\\".$self->{current_db}."\\".lc $table.".schema";
+    if (-e $schema_path) {
         print "[ERROR] Table '$table' already exists!\n";
         return 0;
     }
     
-    sysopen(my $fh, $path, O_WRONLY | O_CREAT, 0755) or
-        die "Unable to open ".$path;
+    if (!sysopen($self->{fh}, $schema_path, O_WRONLY | O_CREAT, 0755)) {
+       print "[ERROR] Unable to create table '$table': ".$!."\n";
+       return undef;
+    }
     
     # Write number of columns as a 16-bit unsigned short
-    syswrite($fh, pack('v', scalar(@$schema), 2));
+    syswrite($self->{fh}, pack('S', scalar(@$schema), 2));
     
+    # Foreach column
     foreach my $column (values $schema) {
+        # Write the type of the column as a 16-bit unsidned short
         if (lc $column->{'column_type'} eq "int") {
-            syswrite($fh, pack('v', 0), 2);
+            syswrite($self->{fh}, pack('S', 0), 2);
         }
         if (lc $column->{'column_type'} eq "text") {
-            syswrite($fh, pack('v', 1), 2);
+            syswrite($self->{fh}, pack('S', 1), 2);
         }
         
+        # Write the length of the column name as a 16bit unsigned short
         my $name_len = length $column->{'column_name'};
-        syswrite($fh, pack('v', $name_len), 2);
-        syswrite($fh, lc $column->{'column_name'}, $name_len);
+        syswrite($self->{fh}, pack('S', $name_len), 2);
+        
+        # Write the column name as a series of bytes
+        syswrite($self->{fh}, $column->{'column_name'}, $name_len);
     }
-    close $fh;
+    close $self->{fh};
+    $self->{fh} = undef;
     
-    $path = lc "Databases/".$self->{current_db}."/".$table.".data";
-    sysopen($fh, $path, O_RDONLY | O_CREAT, 0755) or
-        die "Unable to open ".$path;
+    # Create the data file
+    my $path = "Databases/".$self->{current_db}."/".lc $table.".data";
+    if (!sysopen($self->{fh}, $path, O_RDONLY | O_CREAT, 0755)) {
+        print "[ERROR] Unable to create the data file for table '$table': ".$!."\n";
+        unlink $schema_path;
+        return undef;
+    }
     
-    close $fh;
+    close $self->{fh};
+    $self->{fh} = undef;
+    
     print "CREATE TABLE(".scalar(@$schema).")\n";
     return 1;
 }
@@ -104,31 +123,49 @@ sub read_schema($$) {
     
     if (!defined $self->{current_db}) {
         print "[ERROR] No database selected!\n";
-        return 0;
+        return undef;
     }
     
-    my $schema = lc "Databases/".$self->{current_db}."/".$table.".schema";
+    my $schema = "Databases/".$self->{current_db}."/".lc $table.".schema";
     
     # Read the schema from the file
-    sysopen(my $fh, $schema, O_RDONLY, 0755)
-        or die "Unable to open ".$schema;
+    if (!sysopen($self->{fh}, $schema, O_RDONLY, 0755)) {
+        print "[ERROR] Unable to read schema for table '$table' ($schema): ".$!."\n";
+        $self->{fh} = undef;
+        return undef;
+    }
     
     my $num_columns;
-    sysread($fh, $num_columns, 2);
-    $num_columns = unpack('v', $num_columns);
+    if (!defined sysread($self->{fh}, $num_columns, 2)) {
+        return undef;
+    }
+    $num_columns = unpack('S', $num_columns);
     
     my @columns;
     for (my $i = 0; $i < $num_columns; $i++) {
         my ($column_type, $column_name_length, $column_name);
-        sysread($fh, $column_type, 2);
-        sysread($fh, $column_name_length, 2);
-        $column_name_length = unpack('v', $column_name_length);
-        sysread($fh, $column_name, $column_name_length);
+        # Column type (2)
+        if (!defined sysread($self->{fh}, $column_type, 2)) {
+            return undef;
+        }
+        $column_type = unpack('S', $column_type);
         
-        push @columns, {'column_type'=> unpack('v', $column_type),
+        # Column length (2)
+        if (!defined sysread($self->{fh}, $column_name_length, 2)) {
+            return undef;
+        }
+        $column_name_length = unpack('S', $column_name_length);
+        
+        # Column value (length)
+        if (!defined sysread($self->{fh}, $column_name, $column_name_length)){
+            return undef;
+        }
+        
+        push @columns, {'column_type'=> $column_type,
                         'column_name'=> $column_name};
     }
-    close $fh;
+    close $self->{fh};
+    $self->{fh} = undef;
     
     return \@columns;
 }
@@ -138,67 +175,84 @@ sub insert($$$) {
     
     if (!defined $self->{current_db}) {
         print "[ERROR] No database selected!\n";
-        return 0;
+        return undef;
     }
     
-    my $path = lc "Databases/".$self->{current_db}."/".$table.".data";
-    
+    my $path = "Databases/".$self->{current_db}."/".lc $table.".data";
     my $columns = $self->read_schema($table);
+    if (!defined $columns) {
+        print("[ERROR] Unable to read schema '$table': ".$!."\n");
+        return undef;
+    }
+    
     my $num_columns = scalar(@$columns);
     
     # Insert the data at the end of the file
-    my $fh;
-    sysopen($fh, $path, O_WRONLY | O_APPEND, 0755)
-        or die "Unable to open file '$path': ".$!."\n";
+    if (!sysopen($self->{fh}, $path, O_WRONLY | O_APPEND, 0755)) {
+        print "[ERROR] Unable to open file '$path' for writing: ".$!."\n";
+        return undef;
+    }
         
     foreach my $row (@$values) {
         # Deleted flag (2)
-        syswrite($fh, pack('v', 0), 2);
+        syswrite($self->{fh}, pack('S', 0), 2);
+        
         for (my $i = 0; $i < $num_columns; $ i++) {
             # Length(2) Value(4)
             if (@$columns[$i]->{'column_type'} eq 0) {
-                syswrite($fh, pack('v', 4), 2);
-                syswrite($fh, pack('V', @$row[$i]), 4);
+                syswrite($self->{fh}, pack('S', 4), 2);
+                syswrite($self->{fh}, pack('l', @$row[$i]), 4);
             }
             # Length(2) Value(Length)
             if (@$columns[$i]->{'column_type'} eq 1) {
                 my $strlen = length @$row[$i];
-                syswrite($fh, pack('v', $strlen), 2);
-                syswrite($fh, @$row[$i], $strlen);
+                syswrite($self->{fh}, pack('S', $strlen), 2);
+                syswrite($self->{fh}, @$row[$i], $strlen);
             }
         }
     }
     
-    close $fh;
+    close $self->{fh};
+    $self->{fh} = undef;
+    
     my $num_insert = scalar(@$values);
     print "INSERT($num_insert)\n";
     
-    return 1;
+    return $num_insert;
 }
 
 sub read_record($$) {
-    my ($self, $fh, $table_columns) = @_;
+    my ($self, $table_columns) = @_;
+    if (!defined $self->{fh}) {
+        return undef;
+    }
     
     my (%record, $deleted);
-    # Deleted flag (2)
-    my $read = sysread($fh, $deleted, 2);
-    $deleted = unpack('v', $deleted);
+    # Read deleted flag (16 bit unsigned short)
+    my $read = sysread($self->{fh}, $deleted, 2);
+    $deleted = unpack('S', $deleted);
+    
     #End of file
     if ($read eq 0) {
         return undef;
     }
-    print $deleted."\n";
+    
     if (!$deleted) {
         foreach my $column (values @$table_columns) {
             my ($length, $value);
-            sysread($fh, $length, 2);
-            $length = unpack('v', $length);
-            sysread($fh, $value, $length);
+            # Read length of column value (16 bit unsigned short)
+            sysread($self->{fh}, $length, 2);
+            $length = unpack('S', $length);
+            
+            # Read the column value (length bytes)
+            sysread($self->{fh}, $value, $length);
+            
+            # If this is an integer we must unpack (32 bit signed long)
             if ($column->{'column_type'} eq 0) {
-                $value = unpack('V', $value);
+                $value = unpack('l', $value);
             }
             
-            #print $column->{'column_name'}." ".$value."\n";
+            # Add column to result
             $record{$column->{'column_name'}} = $value;
         }
     }
@@ -234,68 +288,82 @@ sub select($$$) {
     }
     
     my $table_columns = $self->read_schema($table);
-    my $path = lc "Databases/".$self->{current_db}."/".$table.".data";
+    if (!defined $table_columns) {
+        print "[ERROR] Error while reading table schema '$table': ".$!."\n";
+        return undef;
+    }
     
-    sysopen(my $fh, $path, O_RDONLY, 0755)
-        or die "Unable to open file '$path': ".$!."\n";
+    my $path = lc "Databases/".$self->{current_db}."/".lc $table.".data";
+    
+    if (!sysopen($self->{fh}, $path, O_RDONLY, 0755)) {
+        print "[ERROR] Error reading table '$table' records: ".$!."\n";
+        return undef;
+    }
     
     print "SELECT\n";
     RECORD:
     while (1) {
-        my $record = $self->read_record($fh, $table_columns);
+        my $record = $self->read_record($table_columns);
         if (!defined $record) {
             last;
         }
-        #if ($self->check_record($record, $table_columns, $where)) {
+        if ($self->check_record($record, $table_columns, $where)) {
             foreach my $column (values @$table_columns) {
                 print $record->{$column->{'column_name'}}." ";
             }
             print "\n";
-        #}
+        }
     }
         
-    close $fh;
+    close $self->{fh};
+    $self->{fh} = undef;
 }
 
 sub delete($$$){
     my ($self, $table, $where) = @_;
     
     my $table_columns = $self->read_schema($table);
+    if (!defined $table_columns) {
+        print "[ERROR] Unable to read table $table schema: ".$!."\n";
+        return undef;
+    }
+    
     my $path = lc "Databases/".$self->{current_db}."/".$table.".data";
-    sysopen(my $fh, $path, O_RDWR, 0755)
-        or die "Unable to open file '$path': ".$!."\n";
+    if (!sysopen($self->{fh}, $path, O_RDWR, 0755)) {
+        print "[ERROR] Unable to open table '$table' for reading: ".$!."\n";
+        return undef;
+    }
     
     while (1) {
-        my $record = $self->read_record($fh, $table_columns);
-        
+        my $record = $self->read_record($table_columns);
         if (!defined $record) {
             last;
         }
         
         if ($self->check_record($record, $table_columns, $where)) {
-            # Deleted flag
+            # Deleted flag (16 bit unsigned short)
             my $offset = 2;
             foreach my $column (values @$table_columns) {
-                # Length(2) + Int Value(4)
+                # Length(16 bit unsigned short) + Int Value(32 bit signed long)
                 if ($column->{'column_type'} eq 0) {
                     $offset += 6;
                 }
-                # Length(2) + String
+                # Length(16 bit unsigned short) + String (length bytes)
                 if ($column->{'column_type'} eq 1) {
                    $offset += 2 + length ($record->{$column->{'column_name'}});
                 }
             }
             
-            my $cur = sysseek($fh, 0, 1);
+            my $cur = sysseek($self->{fh}, 0, SEEK_CUR);
             print $cur." ".$offset."\n";
-            sysseek($fh, $cur - $offset, 0);
-            print syswrite($fh, pack('v', 1), 2)."bytes changed\n";
-            sysseek($fh, $cur, 0);
-            print "Deleting ... "."\n";
+            sysseek($self->{fh}, $cur - $offset, SEEK_SET);
+            print syswrite($self->{fh}, pack('S', 1), 2)." bytes changed\n";
+            sysseek($self->{fh}, $cur, SEEK_SET);
         }
     }
     
-    close $fh;
+    close $self->{fh};
+    $self->{fh} = undef;
     print "DELETE\n";
 }
 
--- DBEngine/Perl/main.pl
+++ DBEngine/Perl/main.pl
@@ -3,15 +3,17 @@ use warnings;
 use PerlDB;
 
 my $db = PerlDB->new();
-$db->create_db("test");
+#$db->create_db("test");
 $db->select_db("test");
-$db->create_table("Person", [{'column_name' => 'id',
-                              'column_type' => 'int'},
-                             {'column_name' => 'name',
-                              'column_type' => 'text'}]);
-$db->insert("person", [[1, "Ivan"],
-                       [2, "Zlatin"],
-                       [3, "Ceco"]]);
+#$db->create_table("Person", [{'column_name' => 'id',
+#                              'column_type' => 'int'},
+#                             {'column_name' => 'name',
+#                              'column_type' => 'text'}]);
+#$db->insert("person", [[1, "Ivan"],
+#                       [2, "Zlatin"],
+#                       [3, "Ceco"]]);
 $db->select("person", {"id"=>1});
+$db->select("person", {"id"=>2});
 $db->delete("person", {"id"=>1});
 $db->select("person", {"id"=>1});
+$db->select("person", {"id"=>2});
