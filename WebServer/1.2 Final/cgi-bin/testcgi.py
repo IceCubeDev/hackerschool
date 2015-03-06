54c714b6e02ada90da022fbf57a32e40db8d37ca
Ivan Dortulov <ivandortulov@yahoo.com>
2015-03-06 16:04:52 +0200

    [WebServer] Started implementing POST request support.

--- WebServer/1.2 Final/cgi-bin/testcgi.py
+++ WebServer/1.2 Final/cgi-bin/testcgi.py
@@ -1,8 +1,13 @@
+__author__ = 'Ivan Dortulov'
+
 import cgi
 
 print("Content-Type: text/plain;charset=utf-8")
 print()
 
 form = cgi.FieldStorage()
-user = form.getfirst("user", "").upper()    # This way it's safe.
+user = form.getfirst("file", "").upper()    # This way it's safe.
 print(user)
+#for i in range(1, 1024 * 1024):
+#    testString = "abcdefg" * 1024
+#    print(testString)
