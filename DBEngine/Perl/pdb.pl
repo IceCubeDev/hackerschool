c777f4878427981e08fef9bd89646de514e37960
IceCubeDev <ivandortulov@yahoo.com>
2015-03-07 05:19:09 +0200

    [CSS_Tabs + DBEngine] Added solution to CSS Tabs task. Added timing code to DBEngine task.

--- DBEngine/Perl/pdb.pl
+++ DBEngine/Perl/pdb.pl
@@ -3,22 +3,67 @@ use warnings;
 use IO::File;
 
 use constant {
-    DELETED => 0,
+    DELETED => 1,
     INTEGER => 0,
     STRING => 1
 };
 
+sub _select($$)  {
+    my ($fh, $key) = @_;
+    
+    #print "SELECT\n";
+    while (!$fh->eof()) {
+        my $record = read_record($fh);
+        if (!defined $record) {
+            next;
+        }
+        
+        if (check_record($record, $key)) {
+            #print_record($record);
+        }
+    }
+}
+
+sub _insert($$) {
+    my ($fh, $values) = @_;
+    write_record($fh, $values);
+}
+
+sub _delete ($$) {
+    my ($fh, $where) = @_;
+    
+    while (!$fh->eof()) {
+        my $start_pos = $fh->tell();
+        my $record = read_record($fh);
+        if (defined $record) {
+            if (check_record($record, $where)) {
+                my $size = $fh->tell();
+                #print("Found record @ ", $size, "\n");
+                $fh->seek($start_pos, SEEK_SET);
+                #print("Seek to: ".$fh->tell()."\n");
+                $fh->write(pack('S', DELETED));
+                $fh->seek($size, SEEK_SET);
+                #print("Seek to: ".$fh->tell()."\n");
+            }
+        }
+    }
+    
+    #print("DELETE\n");
+}
+
+sub _update($$$) {
+    my ($fh, $where, $values) = @_;
+    _delete($fh, $where);
+    _insert($fh, $values);
+}
+
 sub read_record($) {
-    my $fh = @_;
-    my ($deleted, $rec_len, $num_cols, $type, $col_len, $value, @record);
+    my $fh = shift;
+    my ($deleted, $num_cols, $type, $col_len, $value, @record);
     
     # Read deleted flag
     $fh->read($deleted, 2);
     $deleted = unpack('S', $deleted);
-    
-    # Read the record length
-    $fh->read($rec_len, 4);
-    $rec_len = unpack('l', $rec_len);
     
     # Read number of columns (16 unsigned short)
     $fh->read($num_cols, 2);
@@ -51,49 +96,13 @@ sub read_record($) {
 
 sub write_record($$) {
     my ($fh, $record) = @_;
-}
-
-sub _select($$)  {
-    my ($table, $key) = @_;
     
-    my $fh = IO::File->new();
-    if (!$fh->open("< ".lc $table.".data")) {
-        print "[ERROR] Unable to open table '$table': ".$!."\n";
-        return undef;
-    }
-    
-    print "SELECT\n";
-    while (!$fh->eof()) {
-        my $record = read_record($fh);
-        if (!defined $record) {
-            next;
-        }
-        
-        foreach my $column (@$record) {
-            if ($column eq $key) {
-                print $column." ";
-            }
-        }  
-    }
-    
-    $fh->close();
-}
-
-sub insert($$) {
-    my ($table, $values) = @_;
-    
-    my $fh = IO::File->new();
-    if (!$fh->open(">> ".lc $table.".data")) {
-        print "[ERROR] Unable to open table '$table': ".$!."\n";
-        return undef;
-    }
-    
-    # Write the delete flag
+    # Write the delete flag (2 bytes)
     $fh->print(pack('S', !DELETED));
-    # Write number of columns
-    $fh->print(pack('S', scalar(@$values)));
+    # Write number of columns (2 bytes)
+    $fh->print(pack('S', scalar(@$record)));
     
-    foreach my $value (values @$values) {
+    foreach my $value (values $record) {
         if (@$value[0] eq "int") {
             $fh->print(pack('S', INTEGER));
             $fh->print(pack('S', 4));
@@ -106,18 +115,48 @@ sub insert($$) {
             $fh->print(@$value[1]);
         }
     }
-    
-    $fh->close();
 }
 
-sub delete ($$) {
+sub check_record($$) {
+    my $record = shift;
+    my $key = shift;
     
+    foreach my $column (values $record) {
+        if ($column eq $key) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
-#for (my $i = 0; $i < 100; $i ++) {
-#    insert("test", [["int", 1], ["string", "Ivan"]]);
-#}
-#
-#for (my $i = 0; $i < 100; $i ++) {
-#    _select("test", 1);
-#}
+sub print_record($) {
+    my $record = shift;
+    
+    foreach my $column (values $record) {
+        print $column." ";
+    }
+    print("\n");
+}
+
+my $fh = IO::File->new();
+if (!$fh->open("+< test.data")) {
+    print "[ERROR] Unable to open table test: ".$!."\n";
+} else {
+	$fh->autoflush(1);
+	my $start = time;
+	#for (my $i = 0; $i < 40000; $i ++) {
+	#    _insert($fh, [["int", $i], ["int", $i], ["int", $i], ["int", $i], ["int", $i],
+	#                     ["string", "a" x 1000], ["string", "b" x 10000], ["string", "c" x 100000]]);
+	#}
+	
+	for (my $i = 0; $i < 40000; $i ++) {
+		_update($fh, $i, [["int", $i], ["int", $i], ["int", $i], ["int", $i], ["int", $i],
+							 ["string", "a" x 1000], ["string", "b" x 10000], ["string", "c" x 100000]]);
+	} 
+	#for (my $i = 0; $i < 40000; $i ++) {
+	#	_delete($fh, $i);
+	#} 
+	my $duration = time - $start;
+	print("Operation took ".$duration." seconds\n");
+	$fh->close();
+}
